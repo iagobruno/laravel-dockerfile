a5ca8f2a20d9af0c30a3ff0ad76bd0fcc9ad63e0
Iago Bruno <iagobruno.dev@gmail.com>
2023-03-22 10:33:07 -0300

    Add scheduler and queue containers in docker-composer.yml

---
+++ app/Models/Log.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use App\Jobs\ProccessJob;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Log extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'message',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [];
+
+    /**
+     * The event map for the model.
+     */
+    protected $dispatchesEvents = [
+        // 'created' => \App\Events\LogCreated::class,
+    ];
+}
