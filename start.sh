cfef4d81ff23807d6788d45c95c20429000ddcf6
Iago Bruno <iagobruno.dev@gmail.com>
2025-05-07 19:32:37 -0300

    Update max-requests option

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 if [ "$ENV" = "production" ]; then
-    php artisan octane:frankenphp --host=0.0.0.0 --port=$PORT --admin-port=2019 --max-requests=1
+    php artisan octane:frankenphp --host=0.0.0.0 --port=$PORT --admin-port=2019 --max-requests=500
 else
     php artisan serve --host=0.0.0.0 --port=$PORT
 fi
